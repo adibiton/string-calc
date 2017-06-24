999170f2482afb7db72693602e0e15d8b2275b8c
Adi biton <adi.biton@gmail.com>
2017-06-24 16:25:31 +0300

    feat(first-commit): first-commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+const add = require('lib/add');
+const sub = require('lib/sub');
+const mul = require('lib/mul');
+const div = require('lib/div');
+
+
+module.exports = {
+	add,
+	sub,
+	mul,
+	div
+}
