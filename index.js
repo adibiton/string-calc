b33842dc8ef762433d5b121bf40ffe8386ed19b3
Adi biton <adi.biton@gmail.com>
2017-06-24 16:47:29 +0300

    fix(sum): expose sum

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const sum = require('lib/sum');
+const sum = require('lib/sum').sum;
 const sub = require('lib/sub');
 const mul = require('lib/mul');
 const div = require('lib/div');
