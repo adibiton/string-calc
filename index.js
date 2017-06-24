e3e436716d73e7bfd4f8a6849106a2d3a427daef
Adi biton <adi.biton@gmail.com>
2017-06-24 17:23:03 +0300

    fix(index): change path

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
-const { sum } = require('lib/sum');
-const sub = require('lib/sub');
-const mul = require('lib/mul');
-const div = require('lib/div');
-
+const sum = require('./lib/sum').sum;
+const sub = require('./lib/sub');
+const mul = require('./lib/mul');
+const div = require('./lib/div');
 
 module.exports = {
 	sum,
